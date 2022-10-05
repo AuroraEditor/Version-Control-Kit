75aa2867c403e4234d7073d18ac3206750ed29ee
Nanashi Li <nanashili@plexus.dev>
2022-10-05 13:02:57 +0200

    Update Checkout.swift

--- Sources/Version-Control/Services/Models/Client/Checkout.swift
+++ Sources/Version-Control/Services/Models/Client/Checkout.swift
@@ -9,26 +9,23 @@
 
 import Foundation
 
-public struct Checkout {
-
-    /// Check out the given branch.
-    ///
-    /// @param repository - The repository in which the branch checkout should
-    ///                take place
-    /// @param branch - The branch name that should be checked out
-    public func checkoutBranch(directoryURL: URL, branch: String) throws {
-        try ShellClient.live().run(
-            "cd \(directoryURL.relativePath.escapedWhiteSpaces());git checkout \(branch)"
-        )
-    }
-
-    /// Check out the paths at HEAD.
-    public func checkoutPaths(directoryURL: URL, paths: [String]) throws {
-        try ShellClient.live().run(
-            "cd \(directoryURL.relativePath.escapedWhiteSpaces());git checkout HEAD --\(paths)"
-        )
-    }
-
-    /// Check out either stage #2 (ours) or #3 (theirs) for a conflicted file.
-    public func checkoutConflictedFile() {}
+/// Check out the given branch.
+///
+/// @param repository - The repository in which the branch checkout should
+///                take place
+/// @param branch - The branch name that should be checked out
+public func checkoutBranch(directoryURL: URL, branch: String) throws {
+    try ShellClient.live().run(
+        "cd \(directoryURL.relativePath.escapedWhiteSpaces());git checkout \(branch)"
+    )
 }
+
+/// Check out the paths at HEAD.
+public func checkoutPaths(directoryURL: URL, paths: [String]) throws {
+    try ShellClient.live().run(
+        "cd \(directoryURL.relativePath.escapedWhiteSpaces());git checkout HEAD --\(paths)"
+    )
+}
+
+/// Check out either stage #2 (ours) or #3 (theirs) for a conflicted file.
+public func checkoutConflictedFile() {}
