975f4dadba0c7c48f2e2158a796e45f7363819fd
Nanashi Li <nanashili@plexus.dev>
2022-12-05 14:38:01 +0200

    Update Tag.swift

--- Sources/Version-Control/Services/Models/Client/Tag.swift
+++ Sources/Version-Control/Services/Models/Client/Tag.swift
@@ -9,81 +9,78 @@
 
 import Foundation
 
-public struct Tag {
+/// Create a new tag on the given target commit.
+///
+/// @param name - The name of the new tag.
+/// @param targetCommitSha  - The SHA of the commit where the new tag will live on.
+public func createTag(directoryURL: URL,
+                      name: String,
+                      targetCommitSha: String) throws {
+    try ShellClient().run(
+        "cd \(directoryURL.relativePath.escapedWhiteSpaces());git tag -a -m \(name) \(targetCommitSha)"
+    )
+}
 
-    /// Create a new tag on the given target commit.
-    ///
-    /// @param name - The name of the new tag.
-    /// @param targetCommitSha  - The SHA of the commit where the new tag will live on.
-    public func createTag(directoryURL: URL,
-                          name: String,
-                          targetCommitSha: String) throws {
-        try ShellClient().run(
-            "cd \(directoryURL.relativePath.escapedWhiteSpaces());git tag -a -m \(name) \(targetCommitSha) "
-        )
-    }
+/// Delete a Tag
+///
+/// @param name - The name of the tag to delete.
+public func deleteTag(directoryURL: URL, name: String) throws {
+    try ShellClient().run(
+        "cd \(directoryURL.relativePath.escapedWhiteSpaces());git tag -d \(name)"
+    )
+}
 
-    /// Delete a Tag
-    ///
-    /// @param name - The name of the tag to delete.
-    public func deleteTag(directoryURL: URL, name: String) throws {
-        try ShellClient().run(
-            "cd \(directoryURL.relativePath.escapedWhiteSpaces());git tag -d \(name)"
-        )
-    }
+/// Gets all the local tags. Returns a Map with the tag name and the commit it points to.
+public func getAllTags(directoryURL: URL) throws -> [String: String] {
+    let tags = try ShellClient.live().run(
+        "cd \(directoryURL.relativePath.escapedWhiteSpaces());git show-ref --tags -d"
+    )
 
-    /// Gets all the local tags. Returns a Map with the tag name and the commit it points to.
-    public func getAllTags(directoryURL: URL) throws -> [String: String] {
-        let tags = try ShellClient.live().run(
-            "cd \(directoryURL.relativePath.escapedWhiteSpaces());git show-ref --tags -d"
-        )
+    // TODO: Find a way to convert string to dictionary
+    return [:]
+}
 
-        // TODO: Find a way to convert string to dictionary
-        return [:]
-    }
+/// Fetches the tags that will get pushed to the remote repository.
+///
+/// @param remote - The remote to check for unpushed tags
+/// @param branchName  - The branch that will be used on the push command
+public func fetchTagsToPush(directoryURL: URL,
+                            remote: GitRemote,
+                            branchName: String) throws -> [String] {
+    let args: [Any] = [
+        gitNetworkArguments,
+        "push",
+        remote.name,
+        branchName,
+        "--follow-tags",
+        "--dry-run",
+        "--no-verify",
+        "--porcelain"
+    ]
 
-    /// Fetches the tags that will get pushed to the remote repository.
-    ///
-    /// @param remote - The remote to check for unpushed tags
-    /// @param branchName  - The branch that will be used on the push command
-    public func fetchTagsToPush(directoryURL: URL,
-                                remote: GitRemote,
-                                branchName: String) throws -> [String] {
-        let args: [Any] = [
-            gitNetworkArguments,
-            "push",
-            remote.name,
-            branchName,
-            "--follow-tags",
-            "--dry-run",
-            "--no-verify",
-            "--porcelain"
-        ]
+    let result = try ShellClient.live().run(
+        "cd \(directoryURL.relativePath.escapedWhiteSpaces());git \(args)"
+    )
 
-        let result = try ShellClient.live().run(
-            "cd \(directoryURL.relativePath.escapedWhiteSpaces());git \(args)"
-        )
+    let lines = result.split(separator: "\n").map { String($0) }
+    var currentLine = 1
+    var unpushedTags: [String] = []
 
-        let lines = result.split(separator: "\n").map { String($0) }
-        var currentLine = 1
-        var unpushedTags: [String] = []
+    while currentLine < lines.count && lines[currentLine] != "Done" {
+        let line = lines[currentLine]
+        let parts = line
 
-        while currentLine < lines.count && lines[currentLine] != "Done" {
-            let line = lines[currentLine]
-            let parts = line
-
-            if parts.substring(0) == "*" && parts.substring(2) == "[new tag]" {
-                let tagName = parts.substring(1).split(separator: ":").map {
-                    String($0)
-                }
-
-                if !tagName.description.isEmpty {
-                    unpushedTags.append(tagName.description.replacingOccurrences(of: "/^refs\\/tags\\//", with: ""))
-                }
+        if parts.substring(0) == "*" && parts.substring(2) == "[new tag]" {
+            let tagName = parts.substring(1).split(separator: ":").map {
+                String($0)
             }
-            currentLine += 1
-        }
 
-        return unpushedTags
+            if !tagName.description.isEmpty {
+                unpushedTags.append(tagName.description.replacingOccurrences(of: "/^refs\\/tags\\//", with: ""))
+            }
+        }
+        currentLine += 1
     }
+
+    return unpushedTags
 }
